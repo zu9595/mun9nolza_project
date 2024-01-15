dd45d8fdc98d2a9c0425186ac9e9f010c69d9734
dmg04144 <dmg04144@naver.com>
2024-01-15 17:46:28 +0900

    01/15

--- mun9nolza/src/main/webapp/js/adminjs/member.js
+++ mun9nolza/src/main/webapp/js/adminjs/member.js
@@ -3,6 +3,8 @@
  */
 $(document).ready(memberList);
 
+
+
 function memberList() {
     fetch("memberList.do", {
         method: "get",
@@ -14,12 +16,30 @@ function memberList() {
             $(res).each((idx, member) => {
 				// tr생성. td생성
 
-				let tr = $('<tr />').append($('<td />').text(member.userId),
+				let tr = $('<tr />').addClass('del').append($('<td />').text(member.userId),
 					$('<td />').text(member.userPw),
-					$('<td />').text(member.userName)
+					$('<td />').text(member.userName),
+					$('<td />').append($('<button />').attr('id', 'delete').text('삭제'))
 				);
 				$('#list').append(tr);
 			})
         })
         .catch(console.error);
 };
+
+function memberDel(userId, userPw) {
+    const promise = fetch("memberDel.do?userId=" + userId + "&userPw="+userPw)
+    const json = promise.json();
+    try{
+		if(json.retCode == 'OK'){
+			alert('삭제됨');
+			memberList();
+						
+		}else if(json.retCode == 'NG'){
+			alert('삭제중 애러');
+		}
+	}catch(err){
+		console.error('예외 => ',err);
+	}
+};
+
