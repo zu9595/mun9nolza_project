e67eb7fe9cbdc51e4f24f8809c6259e8367de9f5
최승민 <@DESKTOP-AOIGN5P>
2024-01-26 02:17:45 +0900

    01/26

--- mun9nolza/src/main/java/com/mun9/cart/service/CartService.java
+++ mun9nolza/src/main/java/com/mun9/cart/service/CartService.java
@@ -6,11 +6,14 @@ import com.mun9.cart.vo.CartVO;
 
 public interface CartService {
 	public List<CartVO> selectCartList(String userId);
+	//public List<CartVO> selectReCartList(String userId,int orderNo);
 	public boolean deleteCartList(CartVO vo);
 	public boolean selectOneCart(CartVO vo);
 	public boolean addCartList(CartVO vo);
+	public boolean addCartsList(CartVO vo);
 	public boolean addCartCheck(CartVO vo);
 	public boolean setCartList(CartVO vo);
 	public boolean changeMyproCnt(CartVO vo);
-	public List<CartVO> resetCartList(String userId);
+	public boolean resetCartList(String userId);
+	public List<CartVO> selectOneCart2(CartVO vo);
 }
--- mun9nolza/src/main/java/com/mun9/cart/serviceImpl/CartServiceImpl.java
+++ mun9nolza/src/main/java/com/mun9/cart/serviceImpl/CartServiceImpl.java
@@ -35,6 +35,11 @@ public class CartServiceImpl implements CartService{
 		// TODO Auto-generated method stub
 		return mapper.addCart(vo) == 1;
 	}
+	@Override
+	public boolean addCartsList(CartVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.addCarts(vo) == 1;
+	}
 
 	@Override
 	public boolean selectOneCart(CartVO vo) {
@@ -42,8 +47,8 @@ public class CartServiceImpl implements CartService{
 
 	}
   @Override
-	public List<CartVO> resetCartList(String userId) {
-		return mapper.resetCartList(userId);
+	public boolean resetCartList(String userId) {
+		return mapper.resetCartList(userId) > 0;
 	}
 
 	@Override
@@ -57,6 +62,12 @@ public class CartServiceImpl implements CartService{
 		// TODO Auto-generated method stub
 		return mapper.setCartList(vo) ==1;
 
+	}
+
+	@Override
+	public List<CartVO> selectOneCart2(CartVO vo) {
+		// TODO Auto-generated method stub
+		return mapper.selectOneCart2(vo);
 	}
 	
 
